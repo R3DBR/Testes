da707123dacfa93f6e885500e9f0a3b185ad18d7
R3DBR <tcartaxo30@gmail.com>
2025-03-22 12:41:00 -0300

    Add files via upload

--- Site1/Comandos.js
+++ Site1/Comandos.js
@@ -1,6 +1,9 @@
 function pegar_resposta() {
     var resposta = prompt("Digite a resposta");
-    if (resposta == "Bem-vindo/a" || resposta == "De nada") {
+    if (resposta == "Bem-vindo/a"){
+        window.location.href = "Fase2.html"
+    }
+    else if (resposta == "De nada"){
         window.location.href = "Fase2.html"
     }
     else if (resposta == "Esta indo bem"){
